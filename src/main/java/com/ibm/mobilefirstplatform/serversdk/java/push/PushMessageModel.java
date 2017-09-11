1e88e014cc8cf912c42720501d2ca751263bd3e9
ManavIBM <manvjina@in.ibm.com>
2017-09-11 15:01:55 +0530

    Added additional settings options for APNS and GCM and also added optional settings for Web (Chrome,Firefox etc.) to support the latest Push Server features (#24)
    * Added functionality for Chrome , ChromAppExtension, Firefox , Safari web browser also added extra optional functionality for APNS and GCM
    
    * Revert copyright
    
    * Removed throws from PushMessageModel
    
    * Removed unused imports and refractored long test method
    
    * Test File commit
    
    * Refractor the code
    
    * Modified javadoc for gcm and changes made in PushMessageModel
    
    * Modified README.md
    
    * Code Review Changes
    
    * Modified README
    
    * Test Changes
    
    * javadoc correction
    
    * Javadoc and Readme changes
    
    * README MOD
    
    * Review comments correction
    
    * Review comments completed
    
    * Review comments correction
    
    * Deleted BuilderFactory , Not required anymore
    
    * Review Comments Changes
    
    * Review
    
    * Review comments
    
    * updated alert exception message
    
    * Review AdddOns
    
    * Review changes
    
    * Formatting
    
    * final review changes
    
    * Test Case
    
    * Notification
    
    * Final Review
    
    * Final Review Changes
    
    * README
    
    * README Update
    
    * Final Changes
    
    * README
    
    * README
    
    * Review Changes
    
    * Review Changes
    
    * README Changes
    
    * Refractored
    
    * README Changes
    
    * README
    
    * Added class level comments
    
    * README
    
    * Javadoc
    
    * Review Changes
    
    * README update
    
    * Added Frankfurt region
    
    * Checking jackson
    
    * Review
    
    * Review
    
    * Review
    
    * Review
    
    * Review
    
    * Review
    
    * Review
    
    * Review
    
    * Review
    
    * Doc Review Comments
    
    * Reveiw
    
    * review
    
    * review
    
    * Review
    
    * test
    
    * Revert "Review"
    
    This reverts commit af2625fb1a3b9c46e5a989ae3ad3063245144c72.
    
    * Review
    
    * Review
    
    * Review
    
    * travis
    
    * Travis
    
    * Javadocs
    
    * Review
    
    * Review
    
    * Adding addon for jdk6
    
    * Removing opendjdk6 as new travis VM defaults to trusty which doesnt support openjdk6

---
+++ src/main/java/com/ibm/mobilefirstplatform/serversdk/java/push/PushMessageModel.java
@@ -0,0 +1,101 @@
+/*
+ *     Copyright 2017 IBM Corp.
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package com.ibm.mobilefirstplatform.serversdk.java.push;
+
+/**
+ * 
+ * Parent modal class for notification message.
+ *
+ */
+public final class PushMessageModel {
+
+	private Message message;
+	private Target target;
+	private Settings settings;
+
+	public final Message getMessage() {
+		return message;
+	}
+
+	public final Target getTarget() {
+		return target;
+	}
+
+	public final Settings getSettings() {
+		return settings;
+	}
+
+	private PushMessageModel(Builder builder) {
+		this.message = builder.message;
+		this.settings = builder.settings;
+		this.target = builder.target;
+	}
+
+	/**
+	 * 
+	 * Builder for {@link PushMessageModel}.
+	 *
+	 */
+	public static class Builder {
+		private Message message;
+		private Target target;
+		private Settings settings;
+
+		/**
+		 * 
+		 * @param message
+		 *            Message object with attributes alert and URL.
+		 * @return The Builder object for calls to be linked.
+		 * 
+		 */
+		public final Builder message(final Message message) {
+			this.message = message;
+			return this;
+		}
+
+		/**
+		 * 
+		 * @param target
+		 *            Target object with attributes deviceIds, userIds,
+		 *            platforms and tagNames.
+		 * @return The Builder object for calls to be linked.
+		 */
+		public final Builder target(final Target target) {
+			this.target = target;
+			return this;
+		}
+
+		/**
+		 * 
+		 * @param settings
+		 *            Settings object with platforms settings like APNS, FCM,
+		 *            Safari, and so on.
+		 * @return The Builder object for calls to be linked.
+		 */
+
+		public final Builder settings(final Settings settings) {
+			this.settings = settings;
+			return this;
+		}
+
+		/**
+		 * 
+		 * @return the {@link PushMessageModel} object.
+		 */
+		public PushMessageModel build() {
+			return new PushMessageModel(this);
+		}
+
+	}
+}
