272e3143c8802f8a7f074dbd4ed324452fd6d599
Anantha Krishnan K G <AnanthaKrish@users.noreply.github.com>
2018-01-15 11:31:16 +0530

    Adde US east region

--- src/main/java/com/ibm/mobilefirstplatform/serversdk/java/push/PushNotifications.java
+++ src/main/java/com/ibm/mobilefirstplatform/serversdk/java/push/PushNotifications.java
@@ -41,7 +41,7 @@ public class PushNotifications {
 	public static final String UK_REGION = ".eu-gb.bluemix.net";
 	public static final String SYDNEY_REGION = ".au-syd.bluemix.net";
 	public static final String FRANKFURT_REGION = ".eu-de.bluemix.net";
-	public static final String WASHINGTON = ".us-east.bluemix.net";
+	public static final String US_EAST_REGION = ".us-east.bluemix.net";
 
 
 	public static final Logger logger = Logger.getLogger(PushNotifications.class.getName());
