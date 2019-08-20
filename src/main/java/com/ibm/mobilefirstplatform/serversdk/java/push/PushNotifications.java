6cf9f8796cd15dda594088982c7d66fafacea805
Ananth <agirijak@in.ibm.com>
2019-08-20 11:45:21 +0530

    removed JP tok

--- src/main/java/com/ibm/mobilefirstplatform/serversdk/java/push/PushNotifications.java
+++ src/main/java/com/ibm/mobilefirstplatform/serversdk/java/push/PushNotifications.java
@@ -54,7 +54,6 @@ public class PushNotifications {
 	public static final String SYDNEY_REGION = ".au-syd.bluemix.net";
 	public static final String FRANKFURT_REGION = ".eu-de.bluemix.net";
 	public static final String US_EAST_REGION = ".us-east.bluemix.net";
-	public static final String JP_TOK = ".jp-tok.bluemix.net";
 
 
 	public static final Logger logger = Logger.getLogger(PushNotifications.class.getName());
@@ -106,12 +105,8 @@ public class PushNotifications {
 		if (overrideServerHost != null) {
 			pushMessageEndpointURL = overrideServerHost + PushConstants.URL + tenantId + PushConstants.API;
 		} else {
-			if (bluemixRegion.equals(JP_TOK)) {
-				pushMessageEndpointURL = PushConstants.JPHOST + PushConstants.URL + tenantId + PushConstants.API;
-			} else {
-				pushMessageEndpointURL = PushConstants.HOST + bluemixRegion + PushConstants.URL + tenantId 
+			pushMessageEndpointURL = PushConstants.HOST + bluemixRegion + PushConstants.URL + tenantId 
 				+ PushConstants.API;
-			}
 		}
 	}
 
